e26ba850f6e28047539c9acba948ddccbdee5a23
Jrm <jrm.ys@foxmail.com>
2023-09-20 14:29:28 +0800

    routing finished

--- router/src/app/app.module.ts
+++ router/src/app/app.module.ts
@@ -33,10 +33,21 @@ export class AppModule {
   // Diagnostic only: inspect router configuration
   constructor(router: Router) {
     // console.log('router>>>', router);
+    console.log('config >>>', router.config);
 
     // Use a custom replacer to display function names in the route configs
-    // const replacer = (key: any, value: { name: any; }) => (typeof value === 'function') ? value.name : value;
+    const replacer = (key: any, value: { name: any; }) => {
+      // console.log('key >>>', key);
+      // console.log('----------------------');
+      // console.log('value >>>', value);
+      // console.log('typeof value >>>', typeof value);
+      // console.log('----------------------');
+      /* if (typeof value === 'function') {
+        console.log('value >>>', value.name);
+      } */
 
-    // console.log('Routes: ', JSON.stringify(router.config, replacer, 2));
+      return (typeof value === 'function') ? value.name : value;
+    }
+    console.log('Routes: ', JSON.stringify(router.config, replacer, 2));
   }
 }
